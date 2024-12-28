c1e37f6225e85ea4a1893582eaa53212024ebcb0
Rija Hasan Abidi <95442492+rijahasan@users.noreply.github.com>
2024-12-29 00:42:48 +0500

    design

--- imkaan/lib/screens/home/homepage.dart
+++ imkaan/lib/screens/home/homepage.dart
@@ -2,170 +2,67 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:imkaan/screens/mental.dart';
 import 'package:imkaan/screens/maternity/searchorupdate.dart';
-// import 'package:imkaan/screens/home/maternity.dart';
 import 'package:imkaan/services/auth.dart';
-// import 'package:imkaan/services/db.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
 
   final AuthService _auth = AuthService();
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//           title: const Text(
-//             'Dashboard',
-//             style: TextStyle(
-//                 fontSize: 25,
-//                 fontWeight: FontWeight.w500,
-//                 color: Color.fromARGB(255, 255, 255, 255),
-//                 fontStyle: FontStyle.italic,
-//                 fontFamily: 'Raleway'),
-//           ),
-//           backgroundColor: Colors.grey[850],
-//           actions: <Widget>[
-//             TextButton.icon(
-//               label: const Text(
-//                 "Log Out",
-//                 style: TextStyle(color: Color.fromARGB(255, 241, 235, 183)),
-//               ),
-//               onPressed: () async {
-//                 await _auth.SignOut();
-//               },
-//               icon: const Icon(Icons.person,
-//                   color: Color.fromARGB(255, 241, 235, 183)),
-//             )
-//           ]),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Center(
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               _buildStylizedButton(
-//                 context: context,
-//                 label: 'Maternity Clinic Data',
-//                 onPressed: () {
-//                   Navigator.push(
-//                     context,
-//                     MaterialPageRoute(
-//                         builder: (context) => const UpdateUserPage()),
-//                         // builder: (context) => const UpdatePatientPage()),
-//                   );
-//                 },
-//               ),
-//               const SizedBox(height: 20),
-//               _buildStylizedButton(
-//                 context: context,
-//                 label: 'Legal Aid Data',
-//                 onPressed: () {
-//                   Navigator.push(
-//                     context,
-//                     MaterialPageRoute(
-//                         builder: (context) => const LegalAidPage()),
-//                   );
-//                 },
-//               ),
-//               const SizedBox(height: 20),
-//               _buildStylizedButton(
-//                 context: context,
-//                 label: 'Admin Settings',
-//                 onPressed: () {
-//                   Navigator.push(
-//                     context,
-//                     MaterialPageRoute(
-//                         builder: (context) => const AdminSettingsPage()),
-//                   );
-//                 },
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-
-//   Widget _buildStylizedButton({
-//     required BuildContext context,
-//     required String label,
-//     required VoidCallback onPressed,
-//   }) {
-//     return InkWell(
-//       onTap: onPressed,
-//       child: Container(
-//         width: 500,
-//         padding: const EdgeInsets.symmetric(vertical: 36.0),
-//         decoration: BoxDecoration(
-//           gradient: const LinearGradient(
-//             colors: [Colors.blueGrey, Colors.grey],
-//             begin: Alignment.topLeft,
-//             end: Alignment.bottomRight,
-//           ),
-//           borderRadius: BorderRadius.circular(40),
-//           boxShadow: const [
-//             BoxShadow(
-//               color: Colors.black26,
-//               offset: Offset(0, 4),
-//               blurRadius: 8.0,
-//             ),
-//           ],
-//         ),
-//         child: Center(
-//           child: Text(
-//             label,
-//             style: const TextStyle(
-//                 fontSize: 35,
-//                 fontWeight: FontWeight.w500,
-//                 color: Color.fromARGB(255, 0, 0, 0),
-//                 fontStyle: FontStyle.italic,
-//                 fontFamily: 'Raleway'),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: const Text(
-            'Dashboard',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: Color.fromARGB(255, 53, 58, 0),
-            ),
-          ),
-          centerTitle: true,
-          backgroundColor: const Color.fromARGB(252, 252, 216, 0),
-          elevation: 4.0,
-          actions: <Widget>[
-            TextButton.icon(
-              label: const Text(
-                "Log Out",
-                style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+        title: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Image.asset(
+                'assets/logo.png',
+                height: 40,
+                color: Colors.black,
+                fit: BoxFit.contain,
               ),
-              onPressed: () async {
-                await _auth.SignOut();
-              },
-              icon: const Icon(Icons.person,
-                  color: Color.fromARGB(255, 255, 255, 255)),
-            )
-          ]),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              flex: 3,
+              child: Text(
+                'Dashboard',
+                style: GoogleFonts.poppins(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+        centerTitle: true,
+        backgroundColor: const Color(0xFFFFCA03),
+        elevation: 4.0,
+        actions: <Widget>[
+          TextButton.icon(
+            label: const Text(
+              "Log Out",
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: () async {
+              await _auth.SignOut();
+            },
+            icon: const Icon(Icons.logout, color: Colors.white),
+          )
+        ],
+      ),
       body: Container(
         width: double.infinity,
-        // color: const Color.fromARGB(255, 132, 121, 0), // Black background
-
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             colors: [
-              // Color.fromARGB(248, 0, 0, 0),
-              Color.fromARGB(249, 255, 255, 255),
-              Color.fromARGB(252, 252, 216, 0),
+              Color(0xFFFFFFFF),
+              Color(0xFFFFCA03),
             ],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
@@ -174,34 +71,38 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Welcome to Imkaan Database Management System!',
-              style: GoogleFonts.poppins(
-                fontSize: 23,
-                fontWeight: FontWeight.bold,
-                color: const Color.fromARGB(255, 43, 47, 0),
-                shadows: [
-                  Shadow(
-                    blurRadius: 10.0,
-                    color: Colors.yellow,
-                    offset: const Offset(2, 3),
-                  ),
-                ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                'Welcome to Imkaan Database Management System!',
+                style: GoogleFonts.poppins(
+                  fontSize: 23,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                  shadows: [
+                    Shadow(
+                      blurRadius: 5.0,
+                      color: const Color(0xFFFFCA03),
+                      offset: const Offset(2, 3),
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.center,
               ),
-              textAlign: TextAlign.center,
             ),
-            const SizedBox(height: 110),
+            const SizedBox(height: 80),
             _buildDashboardButton(
               context,
               label: 'Maternity Clinic',
               icon: Icons.local_hospital,
               color: Colors.pinkAccent,
-              highlightColor: const Color.fromARGB(252, 252, 216, 0),
+              highlightColor: const Color(0xFFFFCA03),
               onPressed: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (context) => const Searchorupdate()),
+                    builder: (context) => const Searchorupdate(),
+                  ),
                 );
               },
             ),
@@ -211,11 +112,13 @@ class HomePage extends StatelessWidget {
               label: 'Mental Health Clinic',
               icon: Icons.psychology,
               color: Colors.teal,
-              highlightColor: const Color.fromARGB(252, 252, 216, 0),
+              highlightColor: const Color(0xFFFFCA03),
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const MentalHealth()),
+                  MaterialPageRoute(
+                    builder: (context) => const MentalHealth(),
+                  ),
                 );
               },
             ),
@@ -238,7 +141,7 @@ class HomePage extends StatelessWidget {
       icon: Icon(icon, size: 28, color: Colors.white),
       label: Text(
         label,
-        style: const TextStyle(
+        style: GoogleFonts.poppins(
           fontSize: 20,
           fontWeight: FontWeight.bold,
           color: Colors.white,
